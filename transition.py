2a90a5173445a1bbc314b146960220c754ec4f99
Thomas Knudsen <knudsen.thomas@gmail.com>
2020-03-25 13:14:19 +0100

    Lidt flere opgaver og mennesker

--- transition.py
+++ transition.py
@@ -3,6 +3,7 @@ import sys
 # dict-of-strings
 Mennesker = {
     'asmei':   'Aslak Meister',
+    'erlys':   'Erik Lysdal',
     'nho':     'Henrik Olsen',
     'kjbri':   'Kjeld Brixen',
     'lrh':     'Lars Hole',
@@ -14,7 +15,12 @@ Mennesker = {
     'thokn':   'Thomas Knudsen',
     'pn':      'Palle Nielsen',
     'rmort':   'Rene Thyregod Andsbjerg',
-    'extern':  'Udenfor GRF',           # Elvis, Helle, Steen, PW, morbo, ...
+
+    'elfle':   'Elvis Flesborg',
+    'hefel':   'Helle Feldthaus',
+    'stsuu':   'Steen Suurballe',
+    'pw':      'Peter West-Nielsen',
+    'morbo':   'Morten Romanow Bøgemose',
 }
 
 # dict-of-strings
@@ -22,6 +28,7 @@ Opgaver = {
 #    Kaldenavn      Beskrivelse
     'plugin':      'Færdiggør QGIS-plugin til observationshåndtering',
     'kons':        'Konsolidering af de mange FIRE repos til en samlet kodebase',
+    'målerCD':     'Hvilke dele af (det der engang hed) MålerCDen, skal FIREgenskabes?',
 
     'ilægning':    'Generel data-ilægningsfunktionalitet',
     'ilæg_niv':    'Ilægning af resultater af almindelig højdenetsvedligeholdelse',
@@ -31,11 +38,19 @@ Opgaver = {
     'valdetest':   'Opsæt Valde-testmiljø der udstiller Helles mellemtabeller',
     'kontrol':     'Stikprøvekontrol af Databasemigrering',
 
+    'kons':        'Konsolidering af kodebasen',
+    'ka_forb':     'Forberedelse af kode- og arkitekturintroworkshop',
+    'ka_intro':    'Kode- og arkitekturintroworkshop',
     'test_mark':   'Afprøvning og tilretning af alm beregningsfunktionalitet, FIRE-mark',
     'materiale':   'Udarbejdelse af kursusmateriale',
     'kursus':      'Afholdelse af kursus i beregning og databaseanvendelse',
-    'videnstrans': 'Vidensoverdragelse omkring systemhåndtering',
     'tilpasning':  'Tilpasning af eksisterende kode, udvikling af ny funktionalitet',
+    'udstilling':  'Inkrementel opdatering af udstillingsdata i GDBank',
+    'videnstrans': 'Vidensoverdragelse omkring systemhåndtering',
+}
+
+Afsluttede = {
+    'valdetest',
 }
 
 # dict-of-strings
@@ -51,19 +66,24 @@ Afhængigheder = {
 #    Opgave        Afhængigheder
     'plugin':      {},
     'ilægning':    {},
+    'målerCD':     {},
 
     'ilæg_niv':    {'ilægning'},
     'ilæg_5D':     {'ilæg_niv'},
     'ilæg_GNSS':   {'ilægning'},
 
-    'valdetest':   {'extern'},
+    'valdetest':   {},
     'kontrol':     {'valdetest'},
 
+    'kons':        {},
+    'ka_forb':     {'kons'},
+    'ka_intro':    {'ka_forb'},
     'test_mark':   {'ilæg_niv'},
     'materiale':   {'test_mark'},
     'kursus':      {'materiale'},
-    'videnstrans': {},
-    'tilpasning':  {'kons'}
+    'videnstrans': {'udstilling'},
+    'tilpasning':  {'kons'},
+    'udstilling':  {'kontrol', 'ilægning'},
 }
 
 # dict-of-sets
@@ -71,23 +91,25 @@ Personafhængigheder = {
 #    Opgave        Personafhængigheder
     'plugin':      {'thokn'},
     'ilægning':    {'kreve', 'thokn'},
+    'målerCD':     {'nho', 'majws', 'thokn'},
 
     'ilæg_niv':    {'asmei'},
     'ilæg_5D':     {'asmei'},
     'ilæg_GNSS':   {'meweb'},
 
-    'valdetest':   {'kreve', 'extern'},
+    'valdetest':   {'kreve', 'elfle'},
     'kontrol':     {'krkel', 'nho', 'lrh'},
 
+    'kons':        {'kreve'},
+    'ka_forb':     {'kreve', 'thokn'},
+    'ka_intro':    {'kreve', 'thokn', 'majws', 'taphs', 'asmei', 'lrh'},
     'test_mark':   {'thokn', 'majws', 'kjbri'},
     'materiale':   {'majws', 'kjbri', 'thokn'},
-    'kursus':      {'materiale'},
-    'videnstrans': {'kreve', 'lrh', 'extern'},
+    'kursus':      {'thokn', 'kreve', 'majws', 'asmei', 'kjbri', 'nho', 'pn', 'rmort', 'lrh', 'erlys'},
+    'videnstrans': {'hefel', 'kreve', 'lrh', 'morbo', 'thokn', 'pw'},
     'tilpasning':  {'taphs', 'kreve', 'majws', 'thokn'},
-    'kons':        {'kreve'},
-    'kursus':      {'thokn', 'kreve', 'majws', 'asmei', 'kjbri', 'nho', 'pn', 'rmort', 'lrh'}
+    'udstilling':  {'hefel', 'kreve', 'morbo', 'thokn', 'stsuu'},
 }
-
 
 
 #------------------------------------------------------------------------------
@@ -96,13 +118,26 @@ class plugin:
     """Færdiggør QGIS-plugin til observationshåndtering
 
     osv. osv. (længere beskrivelse her)
-    osv. osv. (længere beskrivelse her)"""
+    osv. osv. (længere beskrivelse her)
+    """
+
     pass
+
 #------------------------------------------------------------------------------
 class kons:
 #------------------------------------------------------------------------------
     """Konsolidering af de mange FIRE repos til en samlet kodebase"""
+
     pass
+
+#------------------------------------------------------------------------------
+class målerCD:
+#------------------------------------------------------------------------------
+    """Hvilke dele af (det der engang hed) MålerCDen, skal FIREgenskabes?
+
+    Bl.a. laver Henrik en SQLite-fil med "lokaloversigt. Hvad er der ellers?
+    """
+
 #------------------------------------------------------------------------------
 class ilægning:
 #------------------------------------------------------------------------------
@@ -161,15 +196,25 @@ class tilpasning:
 
 
 
-def banner(text):
+def banner(text=""):
     print (80*'-')
     print (text)
-    print (80*'-')
+    if text != "":
+        print (80*'-')
+    else:
+        print ()
 
 if 'opgaver' in sys.argv[1:] or len(sys.argv)<2:
     banner('Opgaver')
     for o in Opgaver:
-        print(f"{o:12}  {Opgaver[o]}")
+        if o not in Afsluttede:
+            print(f"{o:12}  {Opgaver[o]}")
+    banner()
+    banner('Afsluttede opgaver')
+    for o in Opgaver:
+        if o in Afsluttede:
+            print(f"{o:12}  {Opgaver[o]}")
+    banner()
 
 if 'mennesker' in sys.argv[1:] or len(sys.argv)<2:
     banner('Opgaveallokering pr. humanoide')
@@ -177,14 +222,17 @@ if 'mennesker' in sys.argv[1:] or len(sys.argv)<2:
         # print(f"{m:5}  {Mennesker[m]}")
         tasks = set()
         for pa in Personafhængigheder:
-            if m in Personafhængigheder[pa]:
+            if m in Personafhængigheder[pa] and pa not in Afsluttede:
                 tasks |= {pa}
-        print(f"{m:12} {tasks}")
+        if (len(tasks) > 0):
+            print(f"{m:12} {tasks}")
+    banner()
 
 for arg in sys.argv[1:]:
     if arg in Opgaver and  arg in globals():
         banner(arg)
         print(globals()[arg].__doc__)
+    banner()
 
 # med venlig hilsen...
 if 'idiot' in sys.argv[1:]:
